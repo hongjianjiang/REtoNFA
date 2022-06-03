6aa8395c042513a7b743c1cf0dbbf0dbf55f3c56
swordjj <1034060866@qq.com>
2022-06-03 23:25:53 +0800

    reg2nfa

--- LTS.thy
+++ LTS.thy
@@ -23,58 +23,12 @@ that @{term "LTS_is_reachable \<Delta>"} is the reflexive, transitive closure of
 
 inductive LTS_is_reachable :: "(('q, 'a) LTS * ('q * 'q) set) \<Rightarrow> 'q \<Rightarrow> 'a list \<Rightarrow> 'q \<Rightarrow> bool" where
    LTS_Empty[intro!]:"LTS_is_reachable lts q [] q"|
-   LTS_Step1:"(\<exists>q''. (q, q'') \<in> snd lts \<and> LTS_is_reachable lts q'' l q') \<Longrightarrow> LTS_is_reachable lts q l q'" |
+   LTS_Step1[intro!]:"(\<exists>q''. (q, q'') \<in> snd lts \<and> LTS_is_reachable lts q'' l q') \<Longrightarrow> LTS_is_reachable lts q l q'" |
    LTS_Step2[intro!]:"(\<exists>q'' \<sigma>. a \<in> \<sigma> \<and> (q, \<sigma>, q'') \<in> fst lts \<and> LTS_is_reachable lts q'' w q') \<Longrightarrow> LTS_is_reachable lts q (a # w) q'"
 
 
 
-
-lemma DeltLTSlemma1:"LTS_is_reachable l q al y \<Longrightarrow>LTS_is_reachable ({(f u a, v, f w a)| u v w. (u,v,w)\<in> fst l},{(f u a, f w a)| u w .(u,w) \<in> snd l}) (f q a) al (f y a)"
-  proof (induction rule: LTS_is_reachable.induct)  
-    case (LTS_Empty lts q)
-    then show ?case by auto
-  next
-    case (LTS_Step1 q lts l q')
-    then show ?case apply auto
-      by (metis LTS_is_reachable.LTS_Step1 mem_Collect_eq pred_equals_eq2 snd_eqD)
-  next
-    case (LTS_Step2 a q lts w q')
-    then show ?case apply auto by blast
-  qed
-
-
-lemma DeltLTSlemma:"LTS_is_reachable l q al y \<Longrightarrow>LTS_is_reachable ({(f u, v, f w)| u v w. (u,v,w)\<in> fst l},{(f u, f w)| u w .(u,w) \<in> snd l}) (f q) al (f y)"
-  proof (induction rule: LTS_is_reachable.induct)  
-    case (LTS_Empty lts q)
-    then show ?case by auto
-  next
-    case (LTS_Step1 q lts l q')
-    then show ?case apply auto
-      by (metis LTS_is_reachable.LTS_Step1 mem_Collect_eq pred_equals_eq2 snd_eqD)
-  next
-    case (LTS_Step2 a q lts w q')
-    then show ?case apply auto by blast
-  qed
-
-
-lemma joinLTSlemma[simp]:"LTS_is_reachable l q x p \<Longrightarrow>  LTS_is_reachable l p y q''\<Longrightarrow> LTS_is_reachable l q (x@y) q''"
-  proof (induction rule: LTS_is_reachable.induct)
-    case (LTS_Empty lts q)
-    then show ?case apply auto done
-  next
-    case (LTS_Step1 q lts l q')
-    then show ?case apply auto 
-      by (meson LTS_is_reachable.LTS_Step1)
-  next
-    case (LTS_Step2 a q lts w q')
-    then show ?case apply auto done
-  qed
- 
-
-lemma notJoinLTSlemma:"\<forall>p. LTS_is_reachable l q x p \<and> \<not> LTS_is_reachable l p y q'' \<Longrightarrow> \<not> LTS_is_reachable l q (x@y) q''"
-  sorry
-
-lemma subLTSlemma[simp]:"LTS_is_reachable l1 q x y \<Longrightarrow> LTS_is_reachable (fst l1 \<union> l2, snd l1 \<union> l3) q x y"
+lemma subLTSlemma:"LTS_is_reachable l1 q x y \<Longrightarrow> LTS_is_reachable (fst l1 \<union> l2, snd l1 \<union> l3) q x y"
   proof (induction rule: LTS_is_reachable.induct)
     case (LTS_Empty lts q)
     then show ?case by auto
@@ -87,6 +41,35 @@ lemma subLTSlemma[simp]:"LTS_is_reachable l1 q x y \<Longrightarrow> LTS_is_reac
     then show ?case 
       by (smt (z3) LTS_is_reachable.LTS_Step2 UnI1 fst_conv)
   qed
+
+lemma DeltLTSlemma1:"LTS_is_reachable lts q l q' \<Longrightarrow>LTS_is_reachable ({(f q a, va, f q' a)| q va q'. (q, va, q')\<in> fst lts}, {(f q a, f q' a)| q q'. (q, q') \<in> snd lts}) (f q a) l (f q' a)"
+  proof (induction rule: LTS_is_reachable.induct)  
+    case (LTS_Empty lts q)
+    then show ?case by auto
+  next
+    case (LTS_Step1 q lts l q')
+    then show ?case apply auto
+      by blast
+  next
+    case (LTS_Step2 a q lts w q')
+    then show ?case apply auto by blast
+  qed
+
+
+lemma joinLTSlemma:"LTS_is_reachable l q x p \<Longrightarrow>  LTS_is_reachable l p y q''\<Longrightarrow> LTS_is_reachable l q (x@y) q''"
+  proof (induction rule: LTS_is_reachable.induct)
+    case (LTS_Empty lts q)
+    then show ?case apply auto done
+  next
+    case (LTS_Step1 q lts l q')
+    then show ?case by auto 
+  next
+    case (LTS_Step2 a q lts w q')
+    then show ?case apply auto done
+  qed
+ 
+
+
 
 
 end
