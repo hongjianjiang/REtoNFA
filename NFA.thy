634590dfedc75e5370d37d950be7cbcafafe6c44
Hongjian Jiang <32237994+swordJJ@users.noreply.github.com>
2023-02-28 14:02:55 +0800

    proof of alter r1 r2

--- NFA.thy
+++ NFA.thy
@@ -16,22 +16,32 @@ begin
 record ('q,'a) NFA_rec =
   \<Q> :: "'q set"           (* "The set of states" *)
   \<Sigma> :: "'a set"           (* alphabet *)
-  \<Delta> :: "('q,'a) LTS"      (* "The transition relation" *)
-  \<Delta>' :: "('q * 'q) set"      (* "The transition relation" *)
+  \<Delta> :: "(('q,'a) LTS * ('q * 'q) set) set"      (* "The transition relation" *)
   \<I> :: "'q set"            (* "The set of initial states *)
   \<F> :: "'q set"           (* "The set of final states *)
 
 text \<open>Using notions for labelled transition systems, 
 it is easy to define the languages accepted by automata.\<close>
 
+definition reachable_from_node1_to_node2 ::"'q \<Rightarrow> 'q \<Rightarrow>  'a list \<Rightarrow> ('q,'a) LTS * ('q * 'q) set \<Rightarrow>  bool" where
+"reachable_from_node1_to_node2 I F w trans1= LTS_is_reachable (fst trans1) (snd trans1) I w F"
+
+
+definition get_reachable_from_LTS_list :: "('q, 'a) NFA_rec \<Rightarrow> 'a list \<Rightarrow> 'q \<Rightarrow> 'q \<Rightarrow> bool set" where
+  "get_reachable_from_LTS_list \<A> w q q' = (reachable_from_node1_to_node2 q q' w) ` (\<Delta> \<A>)"
+
+fun judge ::"bool set \<Rightarrow> bool" where
+"judge bs = (True \<in> bs)"
+
+
+ 
 definition NFA_accept :: "('q, 'a) NFA_rec \<Rightarrow> 'a list \<Rightarrow> bool" where
-  "NFA_accept \<A> w = (\<exists> q \<in> (\<I> \<A>). \<exists> q' \<in> (\<F> \<A>).
-                  LTS_is_reachable (\<Delta> \<A>) (\<Delta>' \<A>) q w q') "
+"NFA_accept \<A> w = (\<exists> q\<in>(\<I> \<A>) . \<exists> q' \<in> (\<F> \<A>). judge (get_reachable_from_LTS_list \<A> w q q'))"
 
-definition NFA_accept_Q :: "('q, 'a) NFA_rec \<Rightarrow> 'q  \<Rightarrow> 'a list \<Rightarrow> bool" where
+(*definition NFA_accept_Q :: "('q, 'a) NFA_rec \<Rightarrow> 'q  \<Rightarrow> 'a list \<Rightarrow> bool" where
   "NFA_accept_Q \<A> q w = (\<exists> q' \<in> (\<F> \<A>).
-                  LTS_is_reachable (\<Delta> \<A>) (\<Delta>' \<A>) q w q') "
+                  LTS_is_reachable (\<Delta> \<A>) (\<Delta>' \<A>) q w q') "*)
 
-definition \<L> where "\<L> \<A> = {w. NFA_accept \<A> w}"
+definition \<L> where "\<L> \<A> = {w. NFA_accept  \<A> w}"
 
 end
