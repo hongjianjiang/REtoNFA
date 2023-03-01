7297018765dd223385392ccd616e3ab36e6d186d
Hongjian Jiang <32237994+swordJJ@users.noreply.github.com>
2023-03-01 19:34:51 +0800

    lack of proof star

--- LTS.thy
+++ LTS.thy
@@ -1,4 +1,3 @@
-
 theory LTS
 imports Main "HOL.Option"
 begin
@@ -11,8 +10,9 @@ a labeled transition system is a subset of $\mathcal{Q} \times \Sigma
 q')$ is an element of $\Delta$ iff starting in state $q$ the state
 $q'$ can be reached reading the label $\sigma$. \<close>
 
-  
+
 type_synonym ('q,'a) LTS = "('q * 'a set * 'q) set"
+
 
 subsubsection  \<open>Reachability\<close>
 
@@ -20,104 +20,13 @@ text \<open>Often it is enough to consider just the first and last state of
 a path. This leads to the following definition of reachability. Notice, 
 that @{term "LTS_is_reachable \<Delta>"} is the reflexive, transitive closure of @{term \<Delta>}.\<close>
 
-inductive LTS_is_reachable :: "('q, 'a) LTS \<Rightarrow>  ('q * 'q) set \<Rightarrow> 'q \<Rightarrow> 'a list \<Rightarrow> 'q \<Rightarrow> bool" for \<Delta> and \<Delta>' where
-  LTS_Empty[intro!]: "LTS_is_reachable \<Delta> \<Delta>' q [] q" |
-  LTS_Step1: "LTS_is_reachable \<Delta> \<Delta>' q l q'" if "(q, q'') \<in> \<Delta>'" and "LTS_is_reachable \<Delta> \<Delta>' q'' l q'" |
-  LTS_Step2[intro!]: "LTS_is_reachable \<Delta> \<Delta>' q (a # w) q'" if "a \<in> \<sigma>" and "(q, \<sigma>, q'') \<in> \<Delta>" and "LTS_is_reachable \<Delta> \<Delta>' q'' w q'"
+primrec LTS_is_reachable :: "('q, 'a) LTS \<Rightarrow> 'q \<Rightarrow> 'a list \<Rightarrow> 'q \<Rightarrow> bool" where
+   "LTS_is_reachable \<Delta> q [] q' = (q = q' ∨ (q, {}, q') ∈ Δ)"|
+   "LTS_is_reachable \<Delta> q (a # w) q' =
+      (\<exists>q'' \<sigma>. a \<in> \<sigma> \<and> (q, \<sigma>, q'') \<in> \<Delta> \<and> LTS_is_reachable \<Delta> q'' w q')"
 
 
-thm LTS_is_reachable.cases 
-thm LTS_is_reachable.induct
+lemma LChr_lemma :"LTS_is_reachable {(a,{x},b)} a l b ⟹ a≠ b ⟹ l = [x]"
+  by (smt (verit, del_insts) LTS_is_reachable.simps(1) LTS_is_reachable.simps(2) empty_iff old.prod.inject remdups_adj.cases singleton_iff)
 
-lemma removeExtraConstrans: "LTS_is_reachable \<Delta> (insert (e1, e2) \<Delta>') ini l end \<Longrightarrow> \<forall>q \<sigma>. (q, \<sigma>, e1) \<notin> \<Delta> \<Longrightarrow> \<forall>q. (q, e1) \<notin> \<Delta>' \<Longrightarrow> ini \<noteq> e1 \<Longrightarrow> LTS_is_reachable \<Delta> \<Delta>' ini l end"
-  proof (induction rule: LTS_is_reachable.induct)
-    case (LTS_Empty q)
-    then show ?case by auto
-  next
-    case (LTS_Step1 q q'' l q')
-    then show ?case 
-      by (metis LTS_is_reachable.LTS_Step1 insert_iff prod.inject)
-  next
-    case (LTS_Step2 a \<sigma> q q'' w q')
-    then show ?case by auto
-  qed
-
-lemma removeExtraTrans1: "LTS_is_reachable (\<Delta>1 \<union> \<Delta>2) (\<Delta>1' \<union> \<Delta>2') ini l end \<Longrightarrow> \<Delta>1 = \<Delta>2 \<Longrightarrow> \<Delta>1' = \<Delta>2' \<Longrightarrow> LTS_is_reachable \<Delta>1 \<Delta>1' ini l end "
-  by simp
-
-lemma removeExtraTrans2: "LTS_is_reachable (\<Delta>1 \<union> \<Delta>2) (\<Delta>1' \<union> \<Delta>2') ini l end \<Longrightarrow> \<Delta>1 \<noteq> \<Delta>2 \<Longrightarrow> \<Delta>1' = \<Delta>2' \<Longrightarrow> LTS_is_reachable (\<Delta>1 \<union> \<Delta>2) \<Delta>1' ini l end "
-  by simp
-
-lemma removeExtraTrans3: "LTS_is_reachable (\<Delta>1 \<union> \<Delta>2) (\<Delta>1' \<union> \<Delta>2') ini l end \<Longrightarrow> \<Delta>1 \<noteq> \<Delta>2 \<Longrightarrow> \<Delta>1' \<noteq> \<Delta>2' \<Longrightarrow> LTS_is_reachable (\<Delta>1 \<union> \<Delta>2) \<Delta>1' ini l end "
-  sorry
-
-
-lemma Delta1Empty: "LTS_is_reachable d1 d2 p l q \<Longrightarrow> d1 =  {} \<Longrightarrow> l = []"
-  by (induction rule: LTS_is_reachable.induct) auto 
-
-  
-
-lemma subLTSlemma:"LTS_is_reachable \<Delta> \<Delta>' q x y \<Longrightarrow> LTS_is_reachable ( \<Delta> \<union> l1) (\<Delta>' \<union> l2) q x y"
-  proof (induction rule: LTS_is_reachable.induct)
-    case (LTS_Empty q)
-    then show ?case by auto
-  next
-    case (LTS_Step1 q q'' l q')
-    then show ?case 
-      by (meson LTS_is_reachable.LTS_Step1 UnI1)
-  next
-    case (LTS_Step2 a \<sigma> q q'' w q')
-    then show ?case by auto
-  qed
- 
-lemma subLTSlemma1:"LTS_is_reachable \<Delta> \<Delta>' q x y \<Longrightarrow> LTS_is_reachable (\<Delta> \<union> {(f q a, va, f q' a)|q va q'. (q,va,q') \<in> \<Delta>}) (\<Delta>' \<union> l1) q x y"
-  by (simp add: subLTSlemma)
-
-
-lemma DeltLTSlemma1:"LTS_is_reachable \<Delta> \<Delta>' q l q' \<Longrightarrow> LTS_is_reachable ({(f q a, va, f q' a)| q va q'. (q, va, q') \<in> \<Delta>}) ({(f q a, f q' a)| q q'. (q, q') \<in> \<Delta>'}) (f q a) l (f q' a)"
-proof (induction rule: LTS_is_reachable.induct)
-  case (LTS_Empty q)
-  then show ?case by auto
-next
-  case (LTS_Step1 q q'' l q')
-  then show ?case 
-    using LTS_is_reachable.simps by fastforce
-next
-  case (LTS_Step2 a \<sigma> q q'' w q')
-  then show ?case by auto
-qed
-
-
-lemma try1: "LTS_is_reachable  \<Delta> \<Delta>' q l q' \<Longrightarrow>  LTS_is_reachable  \<Delta> (\<Delta>' \<union> a ) q l q'"
-  by (metis subLTSlemma sup.idem)
-
-lemma DeltLTSlemma2:"\<exists>l. LTS_is_reachable \<Delta> \<Delta>' q l q' \<Longrightarrow> \<exists>l. LTS_is_reachable ({(f q a, va, f q' a)| q va q'. (q, va, q') \<in> \<Delta>}) ({(f q a, f q' a)| q q'. (q, q') \<in> \<Delta>'}) (f q a) l (f q' a)"
-  apply(rule exE) apply auto subgoal for la  proof -
-    assume "LTS_is_reachable \<Delta> \<Delta>' q la q'" 
-    then have "LTS_is_reachable {(f q a, va, f q' a) |q va q'. (q, va, q') \<in> \<Delta>} {(f q a, f q' a) |q q'. (q, q') \<in> \<Delta>'} (f q a) la (f q' a)" apply(rule LTS_is_reachable.induct)
-      subgoal by auto
-      subgoal for q q'' l q'
-        using LTS_is_reachable.simps by fastforce
-      subgoal for aa \<sigma> q q'' w q'
-        by auto
-      done
-    then show ?thesis by auto
-  qed
-  done
- 
-
-lemma joinLTSlemma:"LTS_is_reachable  \<Delta> \<Delta>' q x p \<Longrightarrow>  LTS_is_reachable  \<Delta> \<Delta>' p y q''\<Longrightarrow> LTS_is_reachable  \<Delta> \<Delta>' q (x@y) q''"
-proof (induction rule: LTS_is_reachable.induct)
-case (LTS_Empty q)
-then show ?case by auto
-next
-  case (LTS_Step1 q q'' l q')
-then show ?case  by (meson LTS_is_reachable.LTS_Step1)
-next
-  case (LTS_Step2 a \<sigma> q q'' w q')
-  then show ?case by auto
-qed
-
-lemma joinLTSlemma1:"\<exists>x. LTS_is_reachable  \<Delta> \<Delta>' q x p \<Longrightarrow>  \<exists>y. LTS_is_reachable  \<Delta> \<Delta>' p y q''\<Longrightarrow> \<exists>x y. LTS_is_reachable  \<Delta> \<Delta>' q (x@y) q''"
-  by (meson joinLTSlemma)     
 end
--- NFA.thy
+++ NFA.thy
@@ -16,25 +16,34 @@ begin
 record ('q,'a) NFA_rec =
   \<Q> :: "'q set"           (* "The set of states" *)
   \<Sigma> :: "'a set"           (* alphabet *)
-  \<Delta> :: "('q,'a) LTS"      (* "The transition relation" *)
-  \<Delta>' :: "('q * 'q) set"      (* "The transition relation" *)
+  \<Delta> :: "(('q,'a) LTS * 'q * 'q) list set"      (* "The transition relation" *)
   \<I> :: "'q set"            (* "The set of initial states *)
   \<F> :: "'q set"           (* "The set of final states *)
 
 text \<open>Using notions for labelled transition systems, 
 it is easy to define the languages accepted by automata.\<close>
 
-definition NFA_accept :: "('q, 'a) NFA_rec \<Rightarrow> 'a list \<Rightarrow> bool" where
-  "NFA_accept \<A> w = (\<exists> q \<in> (\<I> \<A>). \<exists> q' \<in> (\<F> \<A>).
-                  LTS_is_reachable (\<Delta> \<A>) (\<Delta>' \<A>) q w q') "
+primrec single_LTS_reachable_by_path :: "'a list ⇒ (('q,'a) LTS * 'q * 'q) list  ⇒ bool " where
+"single_LTS_reachable_by_path w []= (w = [])"|
+"single_LTS_reachable_by_path w (x# xs) = (∃p q. (w = p @ q ∧ LTS_is_reachable (fst x) (fst (snd x)) p (snd (snd x)) ∧ single_LTS_reachable_by_path q xs))"
 
-definition NFA_accept_Q :: "('q, 'a) NFA_rec \<Rightarrow> 'q  \<Rightarrow> 'a list \<Rightarrow> bool" where
-  "NFA_accept_Q \<A> q w = (\<exists> q' \<in> (\<F> \<A>).
-                  LTS_is_reachable (\<Delta> \<A>) (\<Delta>' \<A>) q w q') "
+lemma concat_lemma:"single_LTS_reachable_by_path qa x ⟹ single_LTS_reachable_by_path q xa ⟹ single_LTS_reachable_by_path (qa @ q) (x @ xa)"
+  apply(induct x arbitrary:qa) apply auto apply(induct xa arbitrary:q) apply auto 
+  by metis 
+
+
+lemma inverse_concat_lemma: "single_LTS_reachable_by_path q (xa @ y) ⟹ ∃p qa. q = qa @ p ∧ single_LTS_reachable_by_path qa xa ∧ single_LTS_reachable_by_path p y"
+  apply(induct xa arbitrary:q) apply auto 
+  by (metis append.assoc)
+
+fun judge ::"bool set \<Rightarrow> bool" where
+"judge bs = (True \<in> bs)"
+
+lemma ""
+
+definition NFA_accept :: "('a, 'b) NFA_rec \<Rightarrow> 'b list \<Rightarrow> bool" where
+  "NFA_accept \<A> w = judge ((single_LTS_reachable_by_path w) `  (Δ 𝒜))"
 
 definition \<L> where "\<L> \<A> = {w. NFA_accept \<A> w}"
 
-
-definition LQ :: "('q, 'a) NFA_rec => 'q \<Rightarrow> 'a list set" where 
- "LQ \<A> q = {w. NFA_accept_Q \<A> q w}"
 end
